1670f8787a4b1e87609900f97727070ffea33d3a
Andy Leverenz <andy@justalever.com>
2017-11-06 18:30:51 -0600

    Move some prod assets to static directory

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,10 +23,27 @@ const config = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'msapplication-TileColor', content: '#000000' },
+      { name: 'msapplication-TileImage', content: '/ms-icon-144x144.png' },
+      { name: 'theme-color', content: '#000000' },
       { hid: 'description', name: 'description', content: 'Justalever aka Andy Leverenz, freelance web designer Saint Louis. Specializing in web design, development, WordPress, mobile apps, user experience and responsive design.' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'apple-touch-icon', sizes: '57x57', href: '/apple-icon-57x57.png' },
+      { rel: 'apple-touch-icon', sizes: '60x60', href: '/apple-icon-60x60.png' },
+      { rel: 'apple-touch-icon', sizes: '72x72', href: '/apple-icon-72x72.png' },
+      { rel: 'apple-touch-icon', sizes: '72x72', href: '/apple-icon-72x72.png' },
+      { rel: 'apple-touch-icon', sizes: '114x114', href: '/apple-icon-114x114.png' },
+      { rel: 'apple-touch-icon', sizes: '120x120', href: '/apple-icon-120x120.png' },
+      { rel: 'apple-touch-icon', sizes: '144x144', href: '/apple-icon-144x144.png' },
+      { rel: 'apple-touch-icon', sizes: '152x152', href: '/apple-icon-152x152.png' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-icon-180x180.png' },
+      { rel: 'icon', type:'image/png', sizes: '192x192', href: '/android-icon-192x192.png' },
+      { rel: 'icon', type:'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type:'image/png', sizes: '96x96', href: '/favicon-96x96.png' },
+      { rel: 'icon', type:'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+      { rel: 'manifest', href: '/manifest.json' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Lora:400,400i,700' }
 
     ]
